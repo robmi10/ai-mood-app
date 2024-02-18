1e36502979f40c68b4cfa9f6cba0bbbde2074398
robmi10 <89600540+robmi10@users.noreply.github.com>
2024-02-18 09:41:36 +0100

    tweak

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,5 @@
 import { authOptions } from "@/server/auth";
 import NextAuth from "next-auth/next";
 
-export const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };

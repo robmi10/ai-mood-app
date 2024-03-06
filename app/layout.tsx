5d544dcbef0fdee394d0995b6e0509f0ab46cd77
robmi10 <89600540+robmi10@users.noreply.github.com>
2024-03-06 21:15:27 +0100

    add metatag

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { MoodProvider } from "./_components/context/MoodContext";
 import Navbar from "./_components/Navbar";
 import { ThemeProviders } from "./_components/providers";
 import { Toaster } from "@/components/ui/toaster"
+import Head from "next/head";
 
 export default function RootLayout({
   children,
@@ -14,6 +15,9 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <Head>
+        <meta name="viewport" content="viewport-fit=cover" />
+      </Head>
       <body className="bg-gradient-to-r from-indigo-200 to-indigo-300 dark:bg-gradient-to-r dark:from-indigo-400 dark:to-indigo-700 h-full w-full">
         <NextAuthProvider>
           <ThemeProviders>
